03b8ca6b350fddf5b07cf001fd06089070d39408
Ciera Romero-Wright <romerowrightciera@gmail.com>
2023-05-23 11:15:18 -0500

    codeup test -show table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,9 +1,14 @@
-USE codeup_test_db;
+USE codeup_test_db ;
+
+
+
          CREATE TABLE IF NOT EXISTS album (
-         id INT UNSIGNED NOT NULL AUTO_INCREMENT
+         id INT UNSIGNED AUTO_INCREMENT primary key ,
          artist VARCHAR(255),
          name VARCHAR(255),
          release_date INT,
         sales FLOAT,
-        genre VARCHAR(255),
+        genre VARCHAR(255)
 );
+
+
